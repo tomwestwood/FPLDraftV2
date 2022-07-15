e55d3d685f4d8670b4dfa0039beb737e07924d43
Tom Westwood <Tom.Westwood@dotdigital.com>
2022-07-15 10:56:31 +0100

    Lots of changes to styles again. Starting to look like I wanted it to.

--- FPLDraftV2/ClientApp/src/app/draft/draft.component.ts
+++ FPLDraftV2/ClientApp/src/app/draft/draft.component.ts
@@ -43,7 +43,7 @@ export class DraftComponent extends DraftBaseComponent {
         setTimeout(() => {
           this.dialog.open(DraftNextManagerDialogComponent, {
             disableClose: true,
-            data: { draft: this.draft, currentPick: this.currentPick, signingManager: this.draftControllerService.getManagerById(this.currentPick.draft_manager_id) }
+            data: { draft: this.draft, currentPick: this.currentPick, signingManager: this.currentPick ? this.draftControllerService.getManagerById(this.currentPick.draft_manager_id) : undefined }
           });
         }, 1000);
       }
